7b92a64ebd9e4316f53c02aa57a1c20801b83148
XandoBit <x-ando@hotmail.com>
2014-12-19 02:14:45 +0000

    añadiendo test-model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -24,4 +24,24 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+
+  test "email unicio" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password debe tener un minimo de cadena" do
+    @user.pass = @user.password_digest = "a" * 5
+    assert_not @user.valid?
+  end
+
 end
