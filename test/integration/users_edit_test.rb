fd4f09d8c53d14a16be8f7510c4037c1bfd09f57
XandoBit <x-ando@hotmail.com>
2014-12-24 00:12:47 +0000

    test 2 edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -15,4 +15,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                     password_confirmation: "xxxxxx" }
     assert_template 'users/edit'
   end
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "adan"
+    email = "x@bar.com"
+    patch user_path(@user), user: { name:  name,
+                                    email: email,
+                                    password:              "",
+                                    password_confirmation: "" }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name,  name
+    assert_equal @user.email, email
+  end 
 end
