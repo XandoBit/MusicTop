8b5d55569190dfab9d5827e31b30299f6e299dc4
XandoBit <x-ando@hotmail.com>
2014-12-29 00:19:23 +0000

    tests cds

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -1,3 +1,5 @@
 class Cd < ActiveRecord::Base
   belongs_to :user
+  validates :user_id, presence: true
+  validates :descrpcion, presence: true, length: { maximum: 300}
 end
--- test/models/cd_test.rb
+++ test/models/cd_test.rb
@@ -4,4 +4,36 @@ class CdTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should be valid" do
+    assert @cd.valid?
+  end
+
+  test "user id should be present" do
+    @cd.user_id = nil
+    assert_not @cd.valid?
+  end
+
+  test "nombre should be present " do
+    @cd.descripcion = "   "
+    assert_not @cd.valid?
+  end
+
+  test "descripcion should be at most 300 characters" do
+    @cd.descripcion = "a" * 300
+    assert_not @cd.valid?
+  end
+
+   test "grupo should be present " do
+    @cd.grupo = "   "
+    assert_not @cd.valid?
+  end
+   test "enlace should be present " do
+    @cd.enlace = "   "
+    assert_not @cd.valid?
+  end
+   test "titulo should be present " do
+    @cd.titulo = "   "
+    assert_not @cd.valid?
+  end
 end
